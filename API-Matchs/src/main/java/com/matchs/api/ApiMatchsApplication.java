bd863795f7449adb686d53f027b1999ffedea973
stephane.lugand <slu974@hotmail.com>
2022-12-20 16:17:09 +0100

    First commit

---
+++ API-Matchs/src/main/java/com/matchs/api/ApiMatchsApplication.java
@@ -0,0 +1,13 @@
+package com.matchs.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMatchsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMatchsApplication.class, args);
+	}
+
+}
