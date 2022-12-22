772f37a1f73fe98d3be66619b7c44c4138b06395
evancours <evan.cours@gmail.com>
2022-12-22 09:42:56 +0100

    commit

--- API-Matchs/src/main/java/com/matchs/api/ApiMatchsApplication.java
+++ API-Matchs/src/main/java/com/matchs/api/ApiMatchsApplication.java
@@ -3,6 +3,11 @@ package com.matchs.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.matchs.api.Model.Matchs;
+import com.matchs.api.Model.Equipe;
+import com.matchs.api.Model.Resultats;
+import com.matchs.api.Model.Joueurs;
+
 @SpringBootApplication
 public class ApiMatchsApplication {
 
