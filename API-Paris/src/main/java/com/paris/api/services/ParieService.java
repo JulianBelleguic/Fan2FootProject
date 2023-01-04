f05f6cc834c3d7574e0131d5d780c7af380b9b76
mathieu.le-masson <mathieu.le-masson@maformationenligne.bzh>
2023-01-04 10:06:16 +0100

    Add calculChances

--- API-Paris/src/main/java/com/paris/api/services/ParieService.java
+++ API-Paris/src/main/java/com/paris/api/services/ParieService.java
@@ -1,7 +1,9 @@
 package com.paris.api.services;
 
+import com.paris.api.models.CoteModel;
 import com.paris.api.models.ParieModel;
 import com.paris.api.models.info_match;
+import com.paris.api.repository.CoteRepository;
 import com.paris.api.repository.ParieRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,10 +15,13 @@ import java.util.Objects;
 public class ParieService implements Serializable {
 
     private final ParieRepository repository;
+    private final CoteRepository coteRepository;
 
-    public ParieService (ParieRepository repository){
+    public ParieService (ParieRepository repository, CoteRepository coteRepository){
         this.repository = repository;
+        this.coteRepository = coteRepository;
     }
+
 
     public ParieModel findByID(Long id) {
         return this.repository.findById(id).orElse(new ParieModel());
@@ -40,11 +45,21 @@ public class ParieService implements Serializable {
         ParieModel newParie = new ParieModel(null, null, null,null,null);
         assert response != null;
         newParie.setIdMatch(response.getId_match());
-        return null;
+        CoteModel coteA = new CoteModel(null, calculChances(response.getScore_eq1(), response.getScore_eq2(), "A"));
+        CoteModel coteB = new CoteModel(null, calculChances(response.getScore_eq1(), response.getScore_eq2(), "B"));
+        CoteModel coteN = new CoteModel(null, calculChances(response.getScore_eq1(), response.getScore_eq2(), "N"));
+        this.coteRepository.save(coteA);
+        this.coteRepository.save(coteB);
+        this.coteRepository.save(coteN);
+        newParie.setIdCoteA(coteA);
+        newParie.setIdCoteB(coteB);
+        newParie.setIdCoteN(coteN);
+        return this.repository.save(newParie);
     }
 
     private static Float calculChances (Integer score1, Integer score2, String call) {
         Integer scoreA, scoreB;
+        Float result = null;
         if (score1 >= score2) {
             scoreA = score1;
             scoreB = score2;
@@ -55,9 +70,20 @@ public class ParieService implements Serializable {
         }
         int delta = scoreA - scoreB;
 
-        Float chanceNulle = (float) ((50 - (delta / 2)) / 100);
-        Float chanceA = (float) (((50 + (delta / 2)) / 100) * ((50 + (delta / 2)) / 100));
-        Float chanceB = (float) (((50 + (delta / 2)) / 100) * ((50 - (delta / 2)) / 100));
+        float chanceNulle = (float) ((50 - (delta / 2.0)) / 100.0);
+        float chanceA = (float) (((50 + (delta / 2.0)) / 100.0) * ((50 + (delta / 2)) / 100.0));
+        float chanceB = (float) (((50 + (delta / 2.0)) / 100) * ((50 - (delta / 2)) / 100.0));
+
+        if (Objects.equals(call, "A")) {
+            result = 1 / chanceA;
+        }
+        else if (Objects.equals(call, "B")) {
+            result = 1 / chanceB;
+        }
+        else if (Objects.equals(call, "N")) {
+            result = 1 / chanceNulle;
+        }
+        return result;
     }
 
 }
