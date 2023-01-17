a8cd6e7d3891195376d4915f9208b6dfe01b97a1
stephane.lugand <slu974@hotmail.com>
2023-01-17 17:07:38 +0100

    Add parieur validation + Rest Controller return

--- API-Paris/src/main/java/com/paris/api/services/AssoPariParieurService.java
+++ API-Paris/src/main/java/com/paris/api/services/AssoPariParieurService.java
@@ -35,34 +35,30 @@ public class AssoPariParieurService implements Serializable {
     }
 
     public ParierModel parier(Long id_parieur, Long id_parie, double montant, String cote){
-        ParieurModel parieur = parieurService.findParieur(id_parieur);
-        if (parieur.getBalance() > montant)
-        {
-            double gainPotentiel;
-            ParierModel model = new ParierModel();
-            ParieModel pariModel = this.parieService.findByID(id_parie);
-            ParieurModel parieurModel = this.parieurService.findParieur(id_parieur);
-            if ("A".equalsIgnoreCase(cote) || "a".equalsIgnoreCase(cote))
-            {
-                gainPotentiel = montant * pariModel.getCoteA();
-            } else if ("B".equalsIgnoreCase(cote) || "b".equalsIgnoreCase(cote)) {
-                gainPotentiel = montant * pariModel.getCoteB();
-            }
-            else if ("N".equalsIgnoreCase(cote) || "n".equalsIgnoreCase(cote)) {
-                gainPotentiel = montant * pariModel.getCoteN();
-            } else
-            {
-                gainPotentiel = 0.0;
-            }
-            model.setIdParieur(parieurModel.getId());
-            model.setIdParis(pariModel.getId());
-            model.setMontant(montant);
-            model.setCoteChoisie(cote);
-            model.setGainPotentiel(gainPotentiel);
-            return this.repository.save(model);
+        ParieurModel parieurModel = this.parieurService.findParieur(id_parieur);
+        ParieModel pariModel = this.parieService.findByID(id_parie);
+        if (pariModel.getId() != null && parieurModel.getId() != null && parieurModel.getBalance() > montant) {
+                double gainPotentiel;
+                ParierModel model = new ParierModel();
+                if ("A".equalsIgnoreCase(cote) || "a".equalsIgnoreCase(cote)) {
+                    gainPotentiel = montant * pariModel.getCoteA();
+                } else if ("B".equalsIgnoreCase(cote) || "b".equalsIgnoreCase(cote)) {
+                    gainPotentiel = montant * pariModel.getCoteB();
+                } else if ("N".equalsIgnoreCase(cote) || "n".equalsIgnoreCase(cote)) {
+                    gainPotentiel = montant * pariModel.getCoteN();
+                } else {
+                    gainPotentiel = 0.0;
+                }
+                model.setIdParieur(parieurModel.getId());
+                model.setIdParis(pariModel.getId());
+                model.setMontant(montant);
+                model.setCoteChoisie(cote);
+                model.setGainPotentiel(gainPotentiel);
+                return this.repository.save(model);
         }
         else {
-            return new ParierModel();}
+            return new ParierModel();
+        }
     }
 
 }
